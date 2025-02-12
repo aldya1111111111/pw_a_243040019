263312032f07059cc02d084d7dcf1aeb370c2af3
aldya11111111 <aldi.adriansyah2006@gmial.com>
2025-02-12 11:13:19 +0700

    latihan

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan php</title>
+</head>
+<body>
+    <h1><?php echo "Hello world!"; ?></h1>
+    <p>halo, nama saya <?php echo "aldi adriansyah"; ?></p>
+</body>
+</html>
---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,13 @@
+<?php 
+$nama = "Aldi Adriansyah";
+$email = "Aldi@gmail";
+$pekerjaan ="saya seorang cyber security";
+$jurusan = "Teknik Informatika";
+$universitas = "Unpas";
+    echo "<h1>$nama</1>";
+    echo "<h4>$email</4>";
+    echo "<h4>$pekerjaan</4>";
+    echo "<h4>$jurusan</4>";
+    echo "<h4>$universitas</4>";
+    echo "<p>Halo saya $nama, saya seorang $universitas Teknik Informatika Unpas</p>";
+?>
