8fd24b51f4e14755ad119e0e952bcd3e9fa78443
aldya11111111 <aldi.adriansyah2006@gmial.com>
2025-02-12 13:35:13 +0700

    tugas

---
+++ tugas/1a.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <h2><em>"<?php echo "Topi saya bundar, Bundar Topi saya"; ?>"</em></h2>
+</body>
+</html>
